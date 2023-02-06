21ccb7ca33f15eba2ce23c6b813f90976ff7bf71
reka <debreka@gmail.com>
2023-02-06 14:34:02 +0100

    prettify navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import {Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import  Container  from 'react-bootstrap/Container';
-import 'bootstrap/dist/css/bootstrap.min.css';  
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -18,32 +18,24 @@ type NavbarProps = {
 }
 
 
-
 const NavigationBar = (props: NavbarProps) => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-3">
       <Container>
-      {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="me-auto">
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
 
-        {/* <ul className='navbar__list'> */}
-        
-          {/* <Link className='navbar__list__logo' to='/'>LOGO</Link> */}
-          {props.pages.map((page: Page, index: number) => (
-            // <li className='navbar__list__item' key={index} >
-            <Nav.Link  as= {Link} key={index} to = {page.route}>{page.name}
-              
-              {/* <NavLink className="text-decoration-none text-white" to={page.route}>{page.name}
-              </NavLink> */}
-</Nav.Link>
-               
-            // </li>
-          ))}
+            {props.pages.map((page: Page, index: number) => (
+              <Nav.Link as={Link} key={index} to={page.route}>{page.name}
 
-        {/* </ul> */}
-        </Nav>
+
+              </Nav.Link>
+
+            ))}
+
+          </Nav>
         </Navbar.Collapse>
 
       </Container>
